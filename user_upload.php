20b7a79b08d9db4b301eca8eda6dd95682b2d9f7
sunnygreentea <69470328+sunnygreentea@users.noreply.github.com>
2021-01-21 17:00:26 -0500

    Update user_upload.php
    Optimize some functions. Update comments.

--- user_upload.php
+++ user_upload.php
@@ -1,7 +1,16 @@
 <?php
 
+//------------------------------------------------------------------------
+//  This code is executed from the command line, which accepts a CSV file as input and processes the CSV file, the parsed file data is inserted into  a PostgreSQL database.
+//------------------------------------------------------------------------
+
 class Users {
 
+	// Assume database name is "postgres"
+	// To run the code correctly, this may be changed
+	private $db_name = "postgres";
+
+	// All argument from command line
 	private $argumentsArr = array(
 		"file" 			=> false,
 		"create_table" 	=> false,
@@ -12,16 +21,27 @@ class Users {
 		"help" 			=> false
 	);
 
+
+
+	//-----------------------------------------------------------------
+	// Construct
+	//-----------------------------------------------------------------
 	public function __construct () {
 
 	}
 
+	//-----------------------------------------------------------------
+	// Run 
+	//-----------------------------------------------------------------
 	public function run ($argc, $argv) {
 		$this->buildArgumentsArr ($argc, $argv);
-		print_r($this->argumentsArr);
+		//print_r($this->argumentsArr);
 		$this->doit();
 	}
 
+	//-----------------------------------------------------------------
+	// Build arguments array
+	//-----------------------------------------------------------------
 	private function buildArgumentsArr ($argc, $argv) {
 		for ($i=0; $i < $argc; $i++) {
 			
@@ -55,34 +75,77 @@ class Users {
 		}
 	}
 
+	//-----------------------------------------------------------------
+	// DO different work according to different input arguments
+	//-----------------------------------------------------------------
 	private function doit () {
+
+		// Help --help
+		// If the input argument contains "--help", show help list info
 		if ($this->argumentsArr["help"] == true) {
 			$this->help();
 		}
 
-		if($this->argumentsArr["file"]) {
-			$usersArr = $this->parseData();
-			if ($this->argumentsArr["h"]==false || $this->argumentsArr["u"]==false  || $this->argumentsArr["p"]==false){
+		// Create table --create_table
+		// If the input argument contains "--create_table", call function "createTable" to "create" or "drop and create" users table
+		// After creating table, check number of users inserted to make sure table is "freshly" created
+		if ($this->argumentsArr["create_table"] == true) {
+			if ($this->argumentsArr["h"]!=false && $this->argumentsArr["u"]!=false && $this->argumentsArr["p"]!=false){
+				$this->createTable();
+				exit(1);
+			}else {
 				echo "please input the arguments [-u], [-h], [-p]\n";
 				$this->help();
 			}
+		}	
+
+		// Dry run --dry_run
+		// If the input argument contains "--dry_run", do everything except creating table and inserting users into database.
+		// What should do: read csv file and parse data, check data to make data rady to insert
+		if ($this->argumentsArr["dry_run"] == true) {
+			if($this->argumentsArr["file"]) {
+				// Passing argument "false" to indicate NOT insert into database
+				$this->parseAndInsert(false);
+			}
 			else {
-				$this->insertUsers($usersArr);
+				echo "please input the arguments [--file]\n";
+				$this->help();
 			}
 		}
 
-		if ($this->argumentsArr["create_table"] == true) {
-			if ($this->argumentsArr["h"]==false || $this->argumentsArr["u"]==false || $this->argumentsArr["p"]==false){
-				echo "please input the arguments [-u], [-h], [-p]\n";
-				$this->help();
-			}else {
-				$this->createTable();
-				exit(1);
-			}
-			
-		}	
+		// File --file -h host -u username -p password
+		// If the input argument contains "--file", parse data from csv file and insert into database
+		if($this->argumentsArr["file"] && $this->argumentsArr["dry_run"] == false) {
+			// Passing argument "true" to indicate YES insert into database
+			$this->parseAndInsert(true);
+		}
+		
 	}
 
+	//-----------------------------------------------------------------
+	// Parse data and insert into database
+	//-----------------------------------------------------------------
+	private function parseAndInsert ($doInsert) {
+		// Show user data
+		$usersArr = $this->parseData();
+		
+		// if found optoins -h, -u, -p, then insert data into db
+		if ($this->argumentsArr["h"]!=false && $this->argumentsArr["u"]!=false  && $this->argumentsArr["p"]!=false){
+			// insert
+			$this->insertUsers($usersArr, $doInsert);
+			// check
+			$this->getUsers();
+		}
+		// else show help info
+		else {
+			echo "please input the arguments [-u], [-h], [-p]\n";
+			$this->help();
+		}
+	}
+
+	//-----------------------------------------------------------------
+	// Output help list info
+	//-----------------------------------------------------------------
 	private function help () {
 		echo "--file [csv file name] – this is the name of the CSV to be parsed.\n";
 		echo "--create_table – this will cause the PostgreSQL users table to be built (and no further actionw will be taken).\n";
@@ -91,10 +154,14 @@ class Users {
 		echo "-p – PostgreSQL password.\n";
 		echo "-h – PostgreSQL host.\n";
 		echo "--help – which will output the above list of directives with details.\n";
-		$this->getUsers();
+		
 		exit(-1);
 	}
 
+
+	//-----------------------------------------------------------------
+	// Parse data from csv
+	//-----------------------------------------------------------------
 	private function parseData () {
 
 		$usersArr = array();
@@ -121,25 +188,42 @@ class Users {
 		        echo "Error: unexpected fgets() fail\n";
 		    }
 		    fclose($file);
-
-		    var_dump($usersArr);
+		    echo "\nData extracted from file: ".$this->argumentsArr["file"]."\n";
+		    print_r($usersArr);
+		    echo "\n";
 		    return $usersArr;
 		}
 	}
 
+	//-----------------------------------------------------------------
+	// Create or recreate table
+	//-----------------------------------------------------------------
 	private function createTable() {
 		$conn = $this->connectDB();		
-		$query = "DROP TABLE IF EXISTS users;
+		
+		// Create table
+		$sql = "DROP TABLE IF EXISTS users;
 				CREATE TABLE users (
 	            id SERIAL PRIMARY KEY,
 	            name VARCHAR(100) NOT NULL,
 	            surname VARCHAR(100) NOT NULL,
 	            email VARCHAR(255) UNIQUE);";
-	    pg_query($conn, $query);
+	    $result = pg_query($conn, $sql);
+
+	    if($result) { 
+	    	echo "Table users created.\n";
+	    	echo "sql : ".$sql ."\n";
+	    	$this->getUsers();
+	    }
+	    else {
+	    	echo "Failed to create table.\n";
+	    }
 	}
 
 
-
+	//-----------------------------------------------------------------
+	// Connect to database
+	//-----------------------------------------------------------------
 	private function connectDB () {
 		/*
 		$db_host = $this->argumentsArray["h"];
@@ -155,58 +239,74 @@ class Users {
 		$db_handle = pg_pconnect("host=$db_host dbname=$db_name user=$db_user password=$db_password");
 
 		if ($db_handle) {
-			//echo 'Connection attempt succeeded.';
 			return $db_handle;
 		} else {
-			//echo 'Connection attempt failed.';
 		}
 	}
 
-	private function insertUsers ($usersArr) {
-		echo "\n INSERT \n";
+	//-----------------------------------------------------------------
+	// Insert users
+	// argument: $doInsert: if true then do insert, else do not insert
+	//-----------------------------------------------------------------
+	private function insertUsers ($usersArr, $doInsert) {
 		$conn = $this->connectDB();	
+		
 		foreach($usersArr as $user) {
 			$name = ucfirst($this->cleanText($user["name"]));
 			$surname = ucfirst($this->cleanText($user["surname"]));
 			$email =  $this->cleanText($user["email"]);
+			
 			if($this->validateEmail($email)) {
-				echo "validated email:".$name.",".$surname.",",$email."\n";
-				//if ($isRunInsertDB){
-				if (1==1){
-					$sql = "INSERT INTO users (name, surname, email)
-							VALUES ('$name','$surname','$email');";
-					echo "sql ".$sql."\n";
+				echo "Email valid. Ready to insert: ".$name.",".$surname.",",$email."\n";
+				if ($doInsert == true){
+					$sql = "INSERT INTO users (name, surname, email) VALUES ('$name','$surname','$email');";
+					//echo "sql ".$sql."\n";
 					$result = pg_query($conn, $sql);
 					if (!$result){
-						echo "insert a row failed:".$name.",".$surname.",".$email."\n";
+						echo "Insert failed: ".$name.",".$surname.",".$email."\n\n";
 					}
 					else {
-						echo "INSERTED.".$name.",".$surname.",".$email."\n";
+						echo "Inserted: ".$name.",".$surname.",".$email."\n\n";
 					}
 				}
 			}
 			else {
-				echo "invalid email:".$name.",".$surname.",",$email."\n";
+				echo "Email NOT valid:".$name.",".$surname.",",$email."\n\n";
 			}	
 		}
 	}
 
+	//-----------------------------------------------------------------
+	// Get users from database
+	//-----------------------------------------------------------------
 	private function getUsers() {
-		echo "USERS.\n"; 
+		
 		$conn = $this->connectDB();	
 		$sql = "SELECT * FROM users";
 		$result = pg_query($conn, $sql);
-		while($row = pg_fetch_array($result)){
-		 $k = $row['name']." ".$row['surname']." ".$row['email']; 
-
-		 echo $k."\n"; 
+		if(pg_num_rows($result)>0) {
+			echo "\n\nAll users:\n"; 
+			while($row = pg_fetch_array($result)){
+				$k = $row['name']." ".$row['surname']." ".$row['email']; 
+				echo $k."\n"; 
+			}
 		}
+		else {
+			echo "No users yet.\n";
+		}
+		
 	}
 
+	//-----------------------------------------------------------------
+	// Clean text such as name, surname, and email
+	//-----------------------------------------------------------------
 	private function cleanText ($text) {
 		return strtolower(trim(str_replace("'","''", $text)));
 	}
 
+	//-----------------------------------------------------------------
+	// Validte email
+	//-----------------------------------------------------------------
 	private function validateEmail ($email) {
 		if (preg_match('/^([0-9a-zA-Z]([-!\.\w]*[0-9a-zA-Z][\'\!]*)*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/i', $email)) 
     		return true;
@@ -215,6 +315,8 @@ class Users {
 	}
 }
 
+
+//-----------------------------------------------------------------
 $users = new Users();
 $users->run($argc, $argv);
 
